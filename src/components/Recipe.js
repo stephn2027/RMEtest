3fda6c94f5e6051a48eb9b109544424146424d6b
Stephen James Uy <69063695+stephn2027@users.noreply.github.com>
2021-09-11 11:22:35 +0900

    addedrecipeeditform

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React,{ useContext } from "react";
 import IngredienstList from "./IngredientsList";
-
-export default function Recipe(props)
- {
-     const {name,cookTime,servings,instructions,ingredients}=props;
+import { RecipeContext } from "./App";
+export default function Recipe(props){
+     const {handleRecipeDelete} = useContext( RecipeContext );
+     const {id,name,cookTime,servings,instructions,ingredients}=props;
   return (
     <div className="recipe">
       <div className="recipe__header">
         <h3 className="recipe__title">{name}</h3>
         <div>
           <button className="btn btn--primary mr-1">Edit</button>
-          <button className="btn btn--danger">Delete</button>
+          <button 
+          className="btn btn--danger"
+          onClick={()=>handleRecipeDelete(id)}
+          >Delete</button>
         </div>
       </div>
       <div className="recipe__row">
---
+++ src/components/RecipeEditIngredient.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function RecipeEditIngredient() {
+  return (
+    <div className="recipe-edit__ingredient">
+      <div>
+        <input type="text" name="" id="" placeholder="add ingredient" />
+        <input type="text" name="" id="" placeholder="amount" />
+        <button>&times;</button>
+      </div>
+
+      
+    </div>
+  );
+}
