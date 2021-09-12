ea8f014dbc24dd5ac793658a28a9acfc94cd6cfe
Stephen James Uy <69063695+stephn2027@users.noreply.github.com>
2021-09-12 21:06:54 +0900

    addedstuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,104 +1,86 @@
-import React, { useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
+import React ,{ useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 import RecipeList from "./RecipeList";
-import RecipeEdit from "./RecipeEdit";
-
-import "../css/app.css";
+import "../css/app.css"
 
 
-export const RecipeContext = React.createContext();
-const LOCAL_STORAGE_KEY = 'cookingWithReact.App[recipes]';
+
+
+
 function App() {
-
-  const [recipes, setRecipes] = useState(sampleRecipes);
-
-  const recipeContextValue ={
-    handleRecipeAdd,
-    handleRecipeDelete,
-  };
-
-  useEffect(()=>{
-    const recipeKeyFromLocalStorage =  localStorage.getItem(LOCAL_STORAGE_KEY);
-    recipeKeyFromLocalStorage===null?alert("no data in local storage"):setRecipes(JSON.parse(recipeKeyFromLocalStorage));
-  },[]);
-
-  useEffect(()=>{
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes));
-  },[recipes]);
-
   
-  
+  const [recipes,setRecipes] = useState(sampleRecipes);
 
-  
-  function handleRecipeAdd() {
+  function handleRecipeAdd(){
     const newRecipe = {
       id: uuidv4(),
-      name: "New",
-      cookTime: "1:00",
+      name:'New',
+      cookTime:'1:00',
       servings: 1,
       instructions: "Instr.",
-      ingredients: [
+      ingredients:[
         {
-          id: uuidv4(),
-          name: "Name",
-          amount: "1 tbsp",
-        },
-      ],
-    };
-    setRecipes([...recipes, newRecipe]);
-  }
-  function handleRecipeDelete(id){
-    setRecipes(recipes.filter(recipe=>recipe.id!==id))
+        id:uuidv4(), 
+        name:"Name",
+        amount:"1 tbsp" }
+      ]
+    }
+    setRecipes([...recipes, newRecipe])
   }
 
-  return <>
-  <RecipeContext.Provider value={recipeContextValue}>
-      <RecipeList recipes={recipes} />
-      <RecipeEdit/>
-  </RecipeContext.Provider>
-  </>
+  
+  return (
+    <RecipeList 
+    recipes ={recipes}
+    handleRecipeAdd = {handleRecipeAdd}
+    />
+  )
+
+  
+  
 }
+
+
 
 const sampleRecipes = [
   {
     id: uuidv4(),
-    name: "Plain Chiken",
-    cookTime: "1:45",
-    servings: "3",
+    name:"Plain Chiken",
+    cookTime:"1:45",
+    servings:"3",
     instructions: `1.Put salt on chicken \n 2.Put chicken inside the oven \n3.Garnish with greens and serve`,
     ingredients: [
       {
-        id: uuidv4(),
-        name: "Chicken",
-        amount: "2 pounds",
+        id:uuidv4(),
+        name:"Chicken",
+        amount:"2 pounds"
       },
       {
-        id: uuidv4(),
-        name: "Salt",
-        amount: "1 tsp",
+      id:uuidv4(),
+      name:"Salt",
+      amount:"1 tsp"
       },
-    ],
+    ]
   },
   {
     id: uuidv4(),
-    name: "Pork Chop",
-    cookTime: "2:45",
-    servings: "2",
-    instructions:
-      "1.Put salt on pork \n 2.Put put inside the oven \n 3.Garnish with greens and serve",
+    name:"Pork Chop",
+    cookTime:"2:45",
+    servings:"2",
+    instructions: "1.Put salt on pork \n 2.Put put inside the oven \n 3.Garnish with greens and serve",
     ingredients: [
       {
-        id: uuidv4(),
-        name: "Pork",
-        amount: "3 pounds",
+        id:uuidv4(),
+        name:"Pork",
+        amount:"3 pounds"
       },
       {
-        id: uuidv4(),
-        name: "Paprika",
-        amount: "1 tsp",
-      },
+        id:uuidv4(),
+        name:"Paprika",
+        amount:"1 tsp"
+        },
     ],
   },
-];
+]
 
 export default App;
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,29 +1,20 @@
-import React, {useContext} from "react";
-import { RecipeContext } from "./App";
+import React from "react";
+
 import Recipe from "./Recipe";
 
-
-export default function RecipeList({recipes}) {
-    const {handleRecipeAdd} = useContext(RecipeContext);
+export default function RecipeList({ recipes, handleRecipeAdd}) {
   return (
-      
     <div className="recipe-list">
       <div>
         {recipes.map((recipe) => {
-          return <Recipe 
-          key={recipe.id} {...recipe} 
-         
-            />
+          return <Recipe key={recipe.id} {...recipe} />;
         })}
       </div>
       
       <div className="recipe-list__add-recipe-btn-container">
       <button onClick={handleRecipeAdd} 
-      className="btn btn--primary"
-      
-      >Add a recipe</button>
+      className="btn btn--primary">Add a recipe</button>
       </div>
     </div>
-    
   );
 }
