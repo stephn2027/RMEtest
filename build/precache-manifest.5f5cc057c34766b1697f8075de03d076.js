b6934630031c77bdda91dd0553d51cdd94c4c29c
stephen <stephn2027@gmail.com>
2022-01-12 11:08:46 +0900

    new build for changes

---
+++ build/precache-manifest.5f5cc057c34766b1697f8075de03d076.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367eea344360b2b11f0e88244848bdcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1dcb600d4e1c928a783",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "46aab271322ed0aa921d",
+    "url": "/static/css/main.19aaf2fb.chunk.css"
+  },
+  {
+    "revision": "b1dcb600d4e1c928a783",
+    "url": "/static/js/2.3e0a0837.chunk.js"
+  },
+  {
+    "revision": "1997bee165156a2e6f6a0b77f38a4170",
+    "url": "/static/js/2.3e0a0837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46aab271322ed0aa921d",
+    "url": "/static/js/main.bd7ec806.chunk.js"
+  },
+  {
+    "revision": "65cecc3bff69e620ef73",
+    "url": "/static/js/runtime-main.c950e6c8.js"
+  },
+  {
+    "revision": "a17abd0604f2ec6f4bafd91d82ef96de",
+    "url": "/static/media/GothamRounded-Bold.a17abd06.otf"
+  },
+  {
+    "revision": "e40570a322c8676a5c07b3630c4cfee5",
+    "url": "/static/media/GothamRounded-Book.e40570a3.otf"
+  },
+  {
+    "revision": "8a29db3245a6f443e85481fa795526df",
+    "url": "/static/media/GothamRounded-LightItalic.8a29db32.otf"
+  }
+]);
